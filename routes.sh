7f492497261d145b806044d24b406c7906a0b670
Henrique Sousa <henriqueldcs@gmail.com>
2019-04-21 18:05:37 -0300

    Script para execucao do projeto

---
+++ routes.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CONT=$(docker ps -aq --filter ancestor=bexs/routes)
+
+if [[ ! -z $CONT ]]
+then
+	echo $(docker stop $CONT)
+fi
+
+./gradlew build
+docker build --force-rm -t bexs/routes .
+docker run --rm -d -p 8080:8080 -v $(pwd)/$1:/resources/input.txt --name routes bexs/routes
+clear
+echo "Starting..."
+./calculate.sh
