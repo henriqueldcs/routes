4b828b29225afc4b5142d5dab837fe01b988f125
Henrique Sousa <henriqueldcs@gmail.com>
2019-05-02 09:52:18 -0300

    Implementação de chamada a api de melhor rota para execução via linha de comandos.

--- src/main/java/br/com/routes/RoutesCommand.java
+++ src/main/java/br/com/routes/RoutesCommand.java
@@ -11,15 +11,35 @@ import java.net.URLConnection;
 public class RoutesCommand {
 
 	public static void main(final String[] args) throws Exception {
-		final URL routes = new URL("http://localhost:8080/routes");
-		final URLConnection uc = routes.openConnection();
-		final BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-						uc.getInputStream()));
-		String inputLine;
 
-		while ((inputLine = in.readLine()) != null)
-			System.out.println(inputLine);
-		in.close();
+		final String urlBestRoute = "http://localhost:8080/routes/console/best/from/%s/to/%s";
+
+		String route;
+		System.out.print("please enter the route:");
+		while (!(route = System.console().readLine()).equals("exit")) {
+
+			final String[] cities = route.split("-");
+			if (cities.length != 2) {
+				System.out.println("Invalid route!!!");
+			} else {
+				try {
+					final URL routes = new URL(String.format(urlBestRoute, cities[0], cities[1]));
+					final URLConnection uc = routes.openConnection();
+					final BufferedReader in = new BufferedReader(
+							new InputStreamReader(
+									uc.getInputStream()));
+
+					String inputLine;
+
+					while ((inputLine = in.readLine()) != null)
+						System.out.println("\t" + inputLine);
+					in.close();
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.print("please enter the route:");
+		}
 	}
 }
