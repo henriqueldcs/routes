1d181cad84e0f7ba6b86053218d8b4d23d34817d
Henrique Sousa <henriqueldcs@gmail.com>
2019-05-02 09:44:25 -0300

    Adição de definição das APIs e Swagger-ui

--- src/main/java/br/com/routes/resources/RoutesResource.java
+++ src/main/java/br/com/routes/resources/RoutesResource.java
@@ -1,26 +1,61 @@
 package br.com.routes.resources;
 
+import br.com.routes.dto.BestRoutePriceDTO;
+import br.com.routes.facades.RouteFacade;
+import br.com.routes.request.NewRouteRequest;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/routes")
+@Api(value = "Rotas", description = "Adição e consulta de rotas.", tags = {"Rotas"})
 public class RoutesResource {
 
-	@GetMapping(produces = APPLICATION_JSON_VALUE)
-	public ResponseEntity getRoutes() throws IOException {
+	@Autowired
+	RouteFacade routeFacade;
 
-		return ResponseEntity.ok().body("Hello Routes");
+	/**
+	 * LOG
+	 */
+	private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+	@GetMapping("/best/from/{from}/to/{to}")
+	@ApiOperation(value = "Encontra melhor rota entre duas cidades.", tags = {"Rotas"})
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Melhor caminho encontrado!", response = BestRoutePriceDTO.class),
+			@ApiResponse(code = 400, message = "Dados obrigatorios não informados!"),
+			@ApiResponse(code = 404, message = "Rota entre as cidades não existe!")
+	})
+	public ResponseEntity<Object> getBestRoute(
+			@ApiParam(name = "from", value = "cidade de origem", required = true) @PathVariable("from") @NotNull final String from,
+			@ApiParam(name = "to", value = "cidade de destino", required = true) @PathVariable("to") @NotNull final String to) {
+
+		logger.info(String.format("Encontra melhor caminho de: %s, para: %s", from, to));
+
+		//TODO
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ApiOperation(value = "Adiciona rota entre duas cidades.", tags = {"Rotas"})
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Rota incluida com sucesso!"),
+			@ApiResponse(code = 400, message = "Dados obrigatorios não informados ou inválidos!"),
+			@ApiResponse(code = 409, message = "Rota entre as cidades já existe!")
+	})
+	public ResponseEntity createRoute(@ApiParam(name = "newRouteRequest", value = "Nova rota", required = true) @RequestBody @Valid final NewRouteRequest newRouteRequest) {
+
+		logger.info(String.format("Adiciona rota: %s", ""));
+
+		//TODO
+
+		return ResponseEntity.notFound().build();
 	}
 }
